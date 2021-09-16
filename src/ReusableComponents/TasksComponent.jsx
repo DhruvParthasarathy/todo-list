4a98ab916c3d15d86fe1bf3969c89460cf25fd64
Dhruv.Parthasarathy <dhruv.parthasarathy@mu-sigma.com>
2021-09-16 14:19:29 +0530

    Added styles

--- src/ReusableComponents/TasksComponent.jsx
+++ src/ReusableComponents/TasksComponent.jsx
@@ -3,20 +3,20 @@ import React, { Component } from "react";
 export default class TasksComponent extends Component {
   render() {
     return (
-      <ul>
+      <ul className="listSection">
         {this.props.tasks.map((task) => (
-          <li id={task.id}>
-            <span>{task.value}</span>
+          <li  id={task.id}>
+            <span className={this.props.type} >{task.value}</span>
             {this.props.type === "pending" ? (
-              <>
+              <div className="buttonsSection">
                 <button onClick={() => this.props.done(task)}>✓</button>
                 <button onClick={() => this.props.remove(task)}>✗</button>
-              </>
+                </div>
             ):
-            <>
+            <div className="buttonsSection">
             <button onClick={() => this.props.undo(task)}>⤹</button>
             
-            </>}
+            </div>}
           </li>
         ))}
       </ul>
